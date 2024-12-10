2f6d5e12c076fb45444482c6a0c567ee35204642
rania <raniaarbash@hotmail.com>
2024-12-10 11:13:17 -0500

    Dec10-V2

---
+++ Dec9Project/app/src/main/java/com/example/dec9project/SecondActivity.java
@@ -0,0 +1,19 @@
+package com.example.dec9project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    // R.java is the auto-generated file that contains all ids for the app resources:
+    // colors, strings, images, fonts, layout , etc...
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_second);
+
+
+    }
+}
+
