8168ec2a1afa2c9c839b19ee567e3e4d68bda0bd
rania <raniaarbash@hotmail.com>
2024-12-10 10:21:05 -0500

    Dec10-V1

---
+++ Dec9Project/app/src/main/java/com/example/dec9project/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.dec9project;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        // R.java is the auto-generated file that contains all ids for the app resources:
+        // colors, strings, images, fonts, layout , etc...
+
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
