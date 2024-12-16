fec4c5c045075f88dd496ec1054fc0a64eebb51d
rania <raniaarbash@hotmail.com>
2024-12-16 15:09:53 -0500

    Dec 16 - List view

--- Dec16/app/src/main/java/com/example/dec16/MainActivity.java
+++ Dec16/app/src/main/java/com/example/dec16/MainActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
+import android.widget.ListView;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -15,20 +17,36 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     Switch colorswitch;
+    ListView collegesList;
     TextView titleText;
     int counter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayList<String> collegsInToronto = new ArrayList<>(0);
+        collegsInToronto.add(getResources().getString(R.string.scollege));
+        collegsInToronto.add(getResources().getString(R.string.hcollege));
+        collegsInToronto.add(getResources().getString(R.string.ccollege));
 
         if (savedInstanceState != null){
-
            counter =  savedInstanceState.getInt("counter");
         }
+
+        collegesList = findViewById(R.id.collegesList);
+        ArrayAdapter adapter = new ArrayAdapter(this,
+                R.layout.row_layout,
+                R.id.collegenameinrow,
+                collegsInToronto);
+        collegesList.setAdapter(adapter);
+
 
         colorswitch = findViewById(R.id.changeColor);
         titleText = findViewById(R.id.titletext);
